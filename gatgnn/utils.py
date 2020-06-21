6e4c1474f0450d2bdfd5a544612d2247e8eb2039
Steph-Yves Louis <slouis@email.sc.edu>
2020-06-21 02:15:29 -0400

    Create utils.py

---
+++ gatgnn/utils.py
@@ -0,0 +1,72 @@
+import numpy as np
+import torch
+import pandas as pd
+import os
+import shutil
+import argparse
+
+from   sklearn.model_selection import train_test_split
+from   sklearn.metrics import mean_absolute_error as sk_MAE
+from   tabulate import tabulate
+import random,time
+
+def set_model_properties(crystal_property):
+    if crystal_property   in ['poisson-ratio','band-gap','absolute-energy','fermi-energy','formation-energy']        :
+        norm_action   = None;classification = None
+    elif crystal_property == 'is_metal':
+        norm_action   = 'classification-1';classification = 1
+    elif crystal_property == 'is_not_metal':
+        norm_action   = 'classification-0';classification = 1
+    else:    
+        norm_action   = 'log';classification = None
+    return norm_action,classification
+
+def torch_MAE(tensor1,tensor2):
+    return torch.mean(torch.abs(tensor1-tensor2))
+
+def torch_accuracy(pred_tensor,true_tensor):
+    _,pred_tensor   = torch.max(pred_tensor,dim=1)
+    correct         = (pred_tensor==true_tensor).sum().float()
+    total           = pred_tensor.size(0)
+    accuracy_ans    = correct/total
+    return accuracy_ans
+
+def output_training(metrics_obj,epoch,estop_val,extra='---'):
+    header_1, header_2 = 'MSE | e-stop','MAE | TIME'
+    if metrics_obj.c_property in ['is_metal','is_not_metal']:
+        header_1,header_2     = 'Cross_E | e-stop','Accuracy | TIME'
+
+    train_1,train_2 = metrics_obj.training_loss1[epoch],metrics_obj.training_loss2[epoch]
+    valid_1,valid_2 = metrics_obj.valid_loss1[epoch],metrics_obj.valid_loss2[epoch]
+    
+    tab_val = [['TRAINING',f'{train_1:.4f}',f'{train_2:.4f}'],['VALIDATION',f'{valid_1:.4f}',f'{valid_2:.4f}'],['E-STOPPING',f'{estop_val}',f'{extra}']]
+    
+    output = tabulate(tab_val,headers= [f'EPOCH # {epoch}',header_1,header_2],tablefmt='fancy_grid')
+    print(output)
+    return output
+
+def load_metrics():
+    saved_metrics = pickle.load(open("MODELS/metrics_.pickle", "rb", -1))
+    return saved_metrics
+
+def make_full_dataset():
+    cgcnn   = pd.read_csv('DATA/cgcnn_data.csv')
+    megnet  = pd.read_csv('DATA/megnet_data.csv')
+
+    full_df = pd.concat([cgcnn,megnet]).drop_duplicates(subset='material_id')
+    full_df = full_df[['pretty_formula', 'fermi_e', 'absolute_e', 'formation_e','shear_moduli', 'poisson_ratio', 'bulk_moduli', 'band_gap','material_id']]
+    full_df.to_csv('DATA/data_set.csv',index=False)
+
+def make_is_metal(threshold=0):
+    '''
+    https://pubs.acs.org/doi/pdf/10.1021/acs.jpclett.8b00124
+    '''
+    data_path      = 'DATA/data_set.csv'
+    df             = pd.read_csv(f'{data_path}')
+    df['is_metal'] = [np.nan]*len(df)
+    df['is_metal'] = np.where((df.band_gap <= threshold),1,df.is_metal)
+    df['is_metal'] = np.where((df.band_gap >  threshold),0,df.is_metal)
+
+    df.to_csv(data_path,index=False)
+    print('> Updated metallic property of data-set')
+    exit()   
